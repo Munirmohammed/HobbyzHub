78b2e5093ae8cb5cd7c928955f343cdb069778af
Bikathi Martin <bikathimartin@outlook.com>
2024-01-19 10:17:40 +0300

    Refactor endpoint controller definitions

--- src/main/java/com/hobbyzhub/javabackend/securitymodule/util/def/controller/AppUserControllerDef.java
+++ src/main/java/com/hobbyzhub/javabackend/securitymodule/util/def/controller/AppUserControllerDef.java
@@ -1,16 +1,14 @@
 package com.hobbyzhub.javabackend.securitymodule.util.def.controller;
 
-import com.hobbyzhub.javabackend.securitymodule.payload.request.GetAccountRequest;
-import com.hobbyzhub.javabackend.securitymodule.payload.request.SendMailRequest;
+import com.hobbyzhub.javabackend.securitymodule.payload.request.OpUserAccountRequest;
 import com.hobbyzhub.javabackend.securitymodule.payload.request.VerifyEmailRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface AppUserControllerDef {
-    ResponseEntity<?> createUserAccount(String email, String userId);
-    ResponseEntity<?> sendOtp(SendMailRequest request, String intent);
+    ResponseEntity<?> sendOtp(OpUserAccountRequest request, String intent);
     ResponseEntity<?> verifyOtp(VerifyEmailRequest request);
-    ResponseEntity<?> getAccountById(GetAccountRequest request);
+    ResponseEntity<?> getAccountById(OpUserAccountRequest request);
     ResponseEntity<?> deleteAccount(String id);
     ResponseEntity<?> updateAccountDetails(String userId, String fullName, String birthdate, String gender, MultipartFile profileImage, String bio);
     ResponseEntity<?> getAccountsList(String page, String size);
