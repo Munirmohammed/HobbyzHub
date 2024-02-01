8ac79aaf0ec68836894436459d1002dc5415dbe3
Munir Mohammed <munirmohammed038@gmail.com>
2024-02-01 19:25:19 +0300

    Merge branch 'dev' into munir working

--- src/main/java/com/hobbyzhub/javabackend/sharedpayload/SharedAccountsInformation.java
+++ src/main/java/com/hobbyzhub/javabackend/sharedpayload/SharedAccountsInformation.java
@@ -13,10 +13,7 @@ public class SharedAccountsInformation {
     private String gender;
     private String bio;
     private String profileImage;
-<<<<<<< HEAD
     private boolean categoryStatus;
-=======
->>>>>>> origin/dev
     private String joinedDate;
     private String birthdate;
 }
